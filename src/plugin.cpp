7acc2dd667d7f00194c3a453553bac84d43d48c9
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-26 06:44:28 -0700

    Setup SKSE project

---
+++ src/plugin.cpp
@@ -0,0 +1,45 @@
+#pragma warning(push)
+
+#include <SKSE/SKSE.h>
+#include <RE/C/ConsoleLog.h>
+
+extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
+    SKSE::Init(skse);
+    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message){
+        if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+            RE::ConsoleLog::GetSingleton()->Print("Hi from .esl-less!");
+        }
+    });
+    return true;
+}
+
+extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* info) {
+    info->infoVersion = SKSE::PluginInfo::kVersion;
+    info->name = "ESPless";
+    info->version = 1;
+    return true;
+}
+
+extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = [](){
+    SKSE::PluginVersionData version;
+    version.PluginName("ESPless");
+    version.PluginVersion({ 0, 0, 1 });
+    version.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+    return version;
+}();
+
+
+
+
+
+
+
+
+//            vm->objectTypeToTypeID()
+
+//            policy->GetHandleForObject(character);
+
+// Find Gold! Then bind a Papyrus script to it!
+//            auto* dataHandler = RE::TESDataHandler::GetSingleton();
+//            auto* gold = dataHandler->LookupForm(0xf, "Skyrim.esm");
+//            gold->
