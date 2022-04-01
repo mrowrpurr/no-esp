80a6b7e735d76fde5f7ecbee7fcef6492fa68763
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-31 19:16:12 -0700

    Enable SKSEPlugin_Version for AE

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -14,7 +14,7 @@ extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadIn
         if (message->type == SKSE::MessagingInterface::kDataLoaded) {
             NoESP::System::ReadAutoBindingsFiles();
             System::ListenForReferences();
-            System::ListenForFirstLocationLoad();
+            System::ListenForFirstLocationLoad(); // for COC support (refactor into something that directly detects COC from main menu)
             SKSE::GetPapyrusInterface()->Register(NoESP::PapyrusInterface::BIND);
         } else if (message->type == SKSE::MessagingInterface::kNewGame || message->type == SKSE::MessagingInterface::kPostLoadGame) {
             auto& system = System::GetSingleton();
@@ -32,11 +32,11 @@ extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::Query
     return true;
 }
 
-//extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = [](){
-//    SKSE::PluginVersionData version;
-//    version.PluginName("NoESP");
-//    version.PluginVersion({ 0, 0, 1 });
-//    version.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-//    version.UsesAddressLibrary(true); // Not really necessary or is it?
-//    return version;
-//}();
+extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = [](){
+    SKSE::PluginVersionData version;
+    version.PluginName("NoESP");
+    version.PluginVersion({ 0, 0, 1 });
+    version.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+    version.UsesAddressLibrary(true); // Not really necessary or is it?
+    return version;
+}();
